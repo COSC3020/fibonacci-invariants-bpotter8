ee7dfb7dcd03fe81a83d674f45fdc9e4f9c2c44a
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-02-22 19:53:11 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,18 +2,18 @@ function fib(n){
 
     let fibArr = [];
 
-    if(n === 0){
-        return 0;
-    }
-
     fibArr[0] = 0;
 
-    if(n === 1){
+    if(n === 0){
         return fibArr;
     }
 
     fibArr[1] = 1;
 
+    if(n === 1){
+        return fibArr;
+    }
+    
     for(let i = 2; i <= n; i++){
         fibArr[i] = fibArr[i - 2] + fibArr[i - 1];
     }
